ce6058fc79b9806fb39a36a578b4989b9e030151
Wannz <42@hiwannz.com>
2026-06-24 11:30:12 +0800

    chore: v0.7.3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -65,7 +65,7 @@ export function Home() {
         ))}
       </div>
 
-      <p className="text-xs text-muted-foreground/50 mt-8">v0.7.2</p>
+      <p className="text-xs text-muted-foreground/50 mt-8">v0.7.3</p>
     </div>
   );
 }
